12dac107ab2d97efef3fb704dfcc1be779baa429
Alex M <inversebit@gmail.com>
2016-12-14 22:24:00 +0100

    Changed header api key var name + return error when it's non-existant or not authorized

--- app/mod_access/utils.py
+++ app/mod_access/utils.py
@@ -4,20 +4,22 @@
 # on the root of the project.
 
 from functools import wraps
-from flask import request, abort
+from flask import request, make_response
 
 from app.mod_access.models import AccessKey
 
-QUERYSTRING_KEY_VAR = 'key'
+QUERYSTRING_KEY_VAR = 'api_key'
 
 
 # The actual decorator function
 def require_appkey(view_function):
     @wraps(view_function)
     def decorated_function(*args, **kwargs):
-        if request.args.get(QUERYSTRING_KEY_VAR):
-            if AccessKey.is_authorized(request.args.get(QUERYSTRING_KEY_VAR)):
+        if request.headers.get(QUERYSTRING_KEY_VAR):
+            if AccessKey.is_authorized(request.headers.get(QUERYSTRING_KEY_VAR)):
                 return view_function(*args, **kwargs)
+            else:
+                return make_response("API key not authorized", 401)
         else:
-            abort(401)
+            return make_response("Must provide an API key", 401)
     return decorated_function
